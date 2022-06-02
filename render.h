e81af9ec42f79d46a8cad06a3e41482667f76c50
GAAAAAAN <lucyk@kookmin.ac.kr>
2022-06-02 20:07:28 +0900

    FIX: Change snake head color from MAGENTA to BLACK
    This commit is just git push test.

--- render.h
+++ render.h
@@ -15,7 +15,7 @@ class Render{
 
   public:
     Render(GameState *gamestate, int map_scale) : gs(gamestate), map_scale(map_scale) {
-      // 생성자 인자는 두개이며, 첫번째 인자는 GameState클래스 포인터, 두번째 인자는 
+      // 생성자 인자는 두개이며, 첫번째 인자는 GameState클래스 포인터, 두번째 인자는
       // 게임 맵의 화면 크기를 나타내는 짝수 정수. 예를 들어 맵의 길이가 10x10인 맵을
       // 출력할 때, map_scale이 2인 경우 화면에서 10x10의 크기로, map_scale이 4인 경우
       // 20x20의 크기로 출력됨.
@@ -41,7 +41,7 @@ class Render{
       init_pair(WALL + 1, COLOR_BLACK, COLOR_BLACK);
       init_pair(EMPTY + 1, COLOR_WHITE, COLOR_WHITE);
       init_pair(SNAKE_BODY + 1, COLOR_YELLOW, COLOR_YELLOW);
-      init_pair(SNAKE_HEAD + 1, COLOR_MAGENTA, COLOR_MAGENTA);
+      init_pair(SNAKE_HEAD + 1, COLOR_BLACK, COLOR_BLACK);
 
       wbkgd(stdscr, COLOR_PAIR(EMPTY + 1));
       // window의 배경 색 설정 (흰색)
@@ -73,17 +73,17 @@ class Render{
                 // 모서리(immune wall)
                 waddch(map_window, ' ' | COLOR_PAIR(IMMUNE_WALL + 1));
                 break;
-              
+
               case WALL:
                 // 벽(wall)
                 waddch(map_window, ' ' | COLOR_PAIR(WALL + 1));
                 break;
-              
+
               case EMPTY:
                 // 빈 칸(empty)
                 waddch(map_window, ' ' | COLOR_PAIR(EMPTY + 1));
                 break;
-              
+
               case SNAKE_BODY:
                 // 뱀 몸통(snake body)
                 waddch(map_window, ' ' | COLOR_PAIR(SNAKE_BODY + 1));
